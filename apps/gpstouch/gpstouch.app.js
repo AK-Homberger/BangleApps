425b091f590483811ec2b44f6122d2563c89f17a
Andreas Koritnik <53138495+AK-Homberger@users.noreply.github.com>
2022-03-31 18:02:42 +0200

    Update gpstouch.app.js

--- apps/gpstouch/gpstouch.app.js
+++ apps/gpstouch/gpstouch.app.js
@@ -94,7 +94,7 @@ const infoData = {
     calc: () => 'SOG: ' + last_fix.speed.toFixed(1) + ' kn',
   },
   ID_COURSE: {
-    calc: () => 'COG: '+ last_fix.course.toFixed(0) +' °',
+    calc: () => 'COG: '+ last_fix.course.toFixed(0) + 'Â°',
   },
   ID_SATS: {
     calc: () => 'HDOP: ' + last_fix.hdop,
