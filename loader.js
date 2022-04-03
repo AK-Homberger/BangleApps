144d0062ed3888d88d41f168729cfa72957a0f30
Andreas Koritnik <53138495+AK-Homberger@users.noreply.github.com>
2022-04-03 12:48:26 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -24,7 +24,7 @@ DEVICEINFO = DEVICEINFO.filter(x=>x.id.startsWith("BANGLEJS"));
 
 // Set up source code URL
 (function() {
-  let username = "ak-homberger";
+  let username = "espruino";
   let githubMatch = window.location.href.match(/\/(\w+)\.github\.io/);
   if (githubMatch) username = githubMatch[1];
   Const.APP_SOURCECODE_URL = `https://github.com/${username}/BangleApps/tree/master/apps`;
