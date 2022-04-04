e84b9ff3ac099c389e24c4050d682111a58c753d
Andreas Koritnik <53138495+AK-Homberger@users.noreply.github.com>
2022-04-05 00:23:13 +0200

    Update blecsc.js

--- apps/cycling/blecsc.js
+++ apps/cycling/blecsc.js
@@ -58,7 +58,7 @@ class BLECSC {
    */
   onValue(event) {
     // Not interested in non-CSC characteristics
-    //if (event.target.uuid != "0x" + MEASUREMENT_UUID) return;
+    if (event.target.uuid != "0x" + MEASUREMENT_UUID) return;
 
     // Notify the generic 'value' handler
     if (this.handlers.value) this.handlers.value(event);
